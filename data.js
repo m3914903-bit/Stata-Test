b056df8c67291c1e0ba2235a2a91b68ef204b25d
m3914903-bit <m3914903@gmail.com>
2026-05-18 11:35:49 +0400

    Update data.js

--- data.js
+++ data.js
@@ -117,5 +117,6 @@ const marketData = [
   { date: "07 may", eur: 0, gbp: 0, aud: 0, cad: 0, chf: 0, jpy: 0, btc: 0, eth: 0, gold: 1, silver: 0, wti: 0, sp500: 0 },// Итого 1 R
   { date: "08 may", eur: 0, gbp: 0, aud: 0, cad: 0, chf: 0, jpy: 0, btc: 0, eth: 0, gold: 1, silver: 0, wti: 0, sp500: 0 },// Итого: 1 R
   { date: "11 may", eur: 0, gbp: 0, aud: 0, cad: 2, chf: 0, jpy: 0, btc: 0, eth: 0, gold: 0, silver: 0, wti: 0, sp500: 0 },
-  { date: "12 may", eur: 0, gbp: 0, aud: 0, cad: 0, chf: 0, jpy: 0, btc: 0, eth: 2, gold: 0, silver: 1, wti: 0, sp500: -1 }
+  { date: "12 may", eur: 0, gbp: 0, aud: 0, cad: 0, chf: 0, jpy: 0, btc: 0, eth: 2, gold: 0, silver: 1, wti: 0, sp500: -1 },
+  { date: "13 may", eur: 0, gbp: 0, aud: 0, cad: 0, chf: 0, jpy: 0, btc: 0, eth: 1, gold: 0, silver: 0, wti: 0, sp500: -1 }
 ];
